4c56a100448b17fa1d2ac9c281c9fe6f2867b42e
tbs <mjczyt@qq.com>
2017-01-04 20:21:40 +0800

    t

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,9 @@ router.get('/', function (req, res, next) {
   console.log("hostname is " + req.hostname);
   switch (req.hostname) {
     case "cqyou.top":
-    console.log(req.path);
-      if (req.path == "/weixin") {
-        res.redirect("http://cqyou.top:2000/weixin");
-        console.log("redirect to http://cqyou.top:2000/weixin")        
-      } else {
-        res.redirect("http://cqyou.top:2000");
-        console.log("redirect to http://cqyou.top:2000")
-      }
+      console.log(req.path);
+      res.redirect("http://cqyou.top:2000");
+      console.log("redirect to http://cqyou.top:2000")
       break;
     case "tbsblog.top":
       res.redirect("http://tbsblog.top:4000");
