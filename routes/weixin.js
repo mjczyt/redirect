295a92c77c55bb7950def10cd65926ca463db294
tbs <mjczyt@qq.com>
2017-01-04 20:24:50 +0800

    n

--- routes/weixin.js
+++ routes/weixin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  console.log(req.params);
+  console.log(req.param("signature"));
 });
 
 module.exports = router;
