2c74b4a59454caedf3044d336bcbb4d9a8376886
tbs <mjczyt@qq.com>
2017-01-04 20:36:27 +0800

    d

--- routes/weixin.js
+++ routes/weixin.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  console.log(req.param("signature"));
+  console.log("http://cqyou.top:2000"+req.originalUrl);
+  
+  res.redirect("http://cqyou.top:2000"+req.originalUrl);
+  
 });
 
 module.exports = router;
