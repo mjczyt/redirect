9d4b3a3f2db2c144720b6b0258189d46a75fa1c2
tbs <mjczyt@qq.com>
2017-01-04 20:13:19 +0800

    test weixin

--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,17 @@ router.get('/', function (req, res, next) {
   console.log("hostname is " + req.hostname);
   switch (req.hostname) {
     case "cqyou.top":
-      res.redirect("http://cqyou.top:2000");
-      console.log("redirect to http://cqyou.top:2000")
+      if (req.path == "/weixin") {
+        res.redirect("http://cqyou.top:2000/weixin");
+        console.log("redirect to http://cqyou.top:2000/weixin")        
+      } else {
+        res.redirect("http://cqyou.top:2000");
+        console.log("redirect to http://cqyou.top:2000")
+      }
       break;
     case "tbsblog.top":
       res.redirect("http://tbsblog.top:4000");
       console.log("redirect to http://tbsblog.top:4000")
-      break;
-    case "118.89.45.55":
-      res.redirect("http://118.89.45.55:3000");
-      console.log("redirect to http://118.89.45.55:3000");
       break;
   }
 });
